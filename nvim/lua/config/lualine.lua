abeabef1f3199e9aa7adc330833fa1a1ae57303c
danielefongo <danielefongo@gmail.com>
2022-10-16 10:07:00 +0200

    neovim: use global theme on lualine

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,5 +1,27 @@
+local theme = require("config.theme")
+local colors = theme.colors
+
+local scheme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.green, gui = 'bold' },
+    b = { fg = colors.white, bg = colors.grey3 },
+    c = { fg = colors.white, bg = colors.grey1 }
+  },
+  command = { a = { fg = colors.black, bg = colors.yellow, gui = 'bold' } },
+  insert = { a = { fg = colors.black, bg = colors.blue, gui = 'bold' } },
+  visual = { a = { fg = colors.black, bg = colors.magenta, gui = 'bold' } },
+  terminal = { a = { fg = colors.black, bg = colors.cyan, gui = 'bold' } },
+  replace = { a = { fg = colors.black, bg = colors.red, gui = 'bold' } },
+  inactive = {
+    a = { fg = colors.grey5, bg = colors.black, gui = 'bold' },
+    b = { fg = colors.grey5, bg = colors.black },
+    c = { fg = colors.grey5, bg = colors.black }
+  }
+}
+
 require('lualine').setup({
   options = {
+    theme = scheme,
     icons_enabled = true,
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
