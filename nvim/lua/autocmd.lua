c672d021eb34d1074d252f46af3ff17986157942
danielefongo <danielefongo@gmail.com>
2023-05-29 12:28:34 +0200

    chore(neovim): remove autocommand for no-name buffer

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -1,16 +1,5 @@
 vim.api.nvim_command("autocmd VimResized * wincmd =")
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*" },
-    command = [[%s/\s\+$//e]],
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    pattern = "{}",
-    callback = function(args)
-      if vim.api.nvim_buf_get_name(args.buf) == "" then
-        vim.opt_local.buftype = "nofile"
-        vim.opt_local.bufhidden = "unload"
-        vim.keymap.set("n", "<leader>d", "", { buffer = args.buf })
-      end
-    end,
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
 })
