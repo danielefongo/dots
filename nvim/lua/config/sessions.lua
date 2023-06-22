9ae3576ab75a53aec85bd3aba9bd767690decbf2
danielefongo <danielefongo@gmail.com>
2023-06-22 08:44:02 +0200

    feat(neovim): remove session-lens

--- nvim/lua/config/sessions.lua
+++ nvim/lua/config/sessions.lua
@@ -1,30 +1,16 @@
 return {
-  {
-    "rmagatti/session-lens",
-    lazy = false,
-    dependencies = {
-      "rmagatti/auto-session",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "plenary.nvim",
-    },
-    config = function()
-      local function close_nvim_tree()
-        vim.cmd("NvimTreeClose")
-      end
+  "rmagatti/auto-session",
+  lazy = false,
+  config = function()
+    local function close_nvim_tree()
+      vim.cmd("NvimTreeClose")
+    end
 
-      require("auto-session").setup({
-        auto_session_use_git_branch = false,
-        auto_session_create_enabled = false,
-        cwd_change_handling = false,
-        pre_save_cmds = { close_nvim_tree },
-      })
-
-      require("session-lens").setup({
-        path_display = { "shorten" },
-        theme_conf = { border = true },
-        previewer = false,
-      })
-    end,
-  },
+    require("auto-session").setup({
+      auto_session_use_git_branch = false,
+      auto_session_create_enabled = false,
+      cwd_change_handling = false,
+      pre_save_cmds = { close_nvim_tree },
+    })
+  end,
 }
