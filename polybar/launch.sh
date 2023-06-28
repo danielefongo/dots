6640d7067a0ee4cf0f40c4739dd78db72cc01edd
danielefongo <danielefongo@gmail.com>
2023-06-28 13:54:41 +0200

    fix(polybar): use xdg config path

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,13 +2,15 @@
 
 killall polybar
 
+CONFIG_FILE=$HOME/.config/polybar/config
+
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 export WLAN=$(ip link show | grep 'wl' | awk -F' ' '{print $2}' | sed 's/://g')
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar top -c $(dirname $0)/config &
-  done
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar top -c $CONFIG_FILE &
+	done
 else
-	polybar top -c $(dirname $0)/config &
+	polybar top -c $CONFIG_FILE &
 fi
