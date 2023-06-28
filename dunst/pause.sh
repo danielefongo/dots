3cc214f13ab79263a87852a1bece369c481a09e6
danielefongo <danielefongo@gmail.com>
2023-06-28 14:26:49 +0200

    improvement(scripts): use "env bash" instead of "bash"

--- dunst/pause.sh
+++ dunst/pause.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bashbash
 
 notify="notify-send -u low dunst"
 
 case $(dunstctl is-paused) in
-    true) dunstctl set-paused false;;
-    false) (dunstctl close && dunstctl set-paused true) &;;
+true) dunstctl set-paused false ;;
+false) (dunstctl close && dunstctl set-paused true) & ;;
 esac
