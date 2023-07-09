d7e060ede6af4d4bca4d97a2e95d49a1b75912bd
danielefongo <danielefongo@gmail.com>
2023-07-09 18:39:54 +0200

    feat: use services instead of running everything with i3

--- dunst/pause.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-notify="notify-send -u low dunst"
-
-case $(dunstctl is-paused) in
-true) dunstctl set-paused false ;;
-false) (dunstctl close && dunstctl set-paused true) & ;;
-esac
