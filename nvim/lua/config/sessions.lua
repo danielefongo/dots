dbbe1a1efb168fe8bfad950a671d420cdfe0a58c
danielefongo <danielefongo@gmail.com>
2023-09-01 09:30:25 +0200

    feat(neovim): update deps

--- nvim/lua/config/sessions.lua
+++ nvim/lua/config/sessions.lua
@@ -2,15 +2,11 @@ return {
   "rmagatti/auto-session",
   lazy = false,
   config = function()
-    local function close_nvim_tree()
-      vim.cmd("NvimTreeClose")
-    end
-
     require("auto-session").setup({
       auto_session_use_git_branch = false,
       auto_session_create_enabled = false,
       cwd_change_handling = false,
-      pre_save_cmds = { close_nvim_tree },
+      pre_save_cmds = { require("nvim-tree.api").tree.close },
     })
   end,
 }
