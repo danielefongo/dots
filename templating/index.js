1f2dcaa0e341491ddda77f1a0d8c381de8da0a5a
danielefongo <danielefongo@gmail.com>
2023-09-08 18:37:11 +0200

    feat(theme): use just one theme

--- templating/index.js
+++ templating/index.js
@@ -4,40 +4,28 @@ const path = require("path");
 const fs = require("fs");
 const childProcess = require("child_process");
 
-const execSync = childProcess.execSync;
 const exec = childProcess.exec;
 
 nunjucks.configure(".", {
   trimBlocks: true,
 });
 
-const defaultSyntax = {
-  statement: "orange",
-  function: "blue",
-  variable: "foreground",
-  include: "magenta",
-  keyword: "magenta",
-  struct: "red",
-  string: "green",
-  identifier: "cyan",
-  field: "cyan",
-  parameter: "red",
-  property: "orange",
-  punctuation: "foreground",
-  constructor: "cyan",
-  operator: "grey8",
-  preproc: "cyan",
-  constant: "orange",
-  tag: "red",
-  todo_fg: "cyan",
-  todo_bg: "grey1",
-  number: "orange",
-  comment: "grey4",
-  type: "yellow",
-  conditional: "red",
+const template = require("./theme.js");
+
+const colors = generateColors(template.palette);
+const scaleRatio = getOrDefault(template.scaleRatio, 1.0);
+
+const data = {
+  colors,
+  scaleRatio,
+  font: getOrDefault(template.font, "Hasklug Nerd Font"),
+  gap: scale(getOrDefault(template.gap, 0), scaleRatio),
+  transparency: getOrDefault(template.transparency, false),
+  round: scale(getOrDefault(template.round, 0), scaleRatio),
+  border: scale(getOrDefault(template.border, 1), scaleRatio),
+  fontSize: scale(getOrDefault(template.fontSize, 12), scaleRatio),
 };
 
-const files = {};
 exec(
   `find . -type f -name "*.template" -mindepth 1 -printf "%P\n"`,
   function(_, stdout, _) {
@@ -50,25 +38,8 @@ exec(
 
         fs.writeFileSync(it.replace(".template", ""), renderedTemplate);
       });
-  }
+  },
 );
-
-const template = require(`../theme/${process.argv.slice(2)[0]}.json`);
-
-const colors = generateColors(template.palette);
-const scaleRatio = getOrDefault(template.scaleRatio, 1.0);
-
-const data = {
-  colors,
-  scaleRatio,
-  syntax: generateSyntax(template.syntax, colors),
-  font: getOrDefault(template.font, "Hasklug Nerd Font"),
-  gap: scale(getOrDefault(template.gap, 0), scaleRatio),
-  transparency: getOrDefault(template.transparency, false),
-  round: scale(getOrDefault(template.round, 0), scaleRatio),
-  border: scale(getOrDefault(template.border, 1), scaleRatio),
-  fontSize: scale(getOrDefault(template.fontSize, 12), scaleRatio),
-};
 
 function getOrDefault(value, fallback) {
   if (value == undefined) {
@@ -102,7 +73,7 @@ function generateColors(palette) {
   colors.background = background;
   colors.background_alt1 = tinycolor(background).darken(3).toHexString();
   colors.background_alt2 = tinycolor(background).lighten(3).toHexString();
-  Array.from({ length: 10 }, (x, i) => i)
+  Array.from({ length: 10 }, (_, i) => i)
     .slice(1)
     .forEach((greyScale) => {
       colors[`grey${greyScale}`] = tinycolor
@@ -112,16 +83,6 @@ function generateColors(palette) {
   colors.foreground = foreground;
 
   return colors;
-}
-
-function generateSyntax(customSyntax, colors) {
-  const syntax = { ...defaultSyntax, ...customSyntax };
-
-  Object.entries(syntax).forEach(([key, value]) => {
-    syntax[key] = colors[value];
-  });
-
-  return syntax;
 }
 
 function scale(value, ratio) {
