73d51437d4e6bfe5713a37c80e540bbe2c2cf967
danielefongo <danielefongo@gmail.com>
2023-10-05 15:40:45 +0200

    feat(neovim): update nvim-tree options

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -4,42 +4,47 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     cmd = { "NvimTreeToggle", "NvimTreeOpen" },
     opts = {
-      update_cwd = true,
-      update_focused_file = {
-        enable = true,
-        update_cwd = false,
-      },
+      sync_root_with_cwd = true,
       view = {
-        adaptive_size = true,
         side = "left",
-        width = 25,
-        hide_root_folder = true,
-      },
-      git = {
-        enable = true,
-        ignore = false,
-      },
-      diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-      },
-      filesystem_watchers = {
-        enable = true,
+        width = {
+          min = 25,
+          max = 75,
+        },
       },
       renderer = {
+        root_folder_label = false,
         highlight_git = true,
-        highlight_opened_files = "none",
-        indent_markers = {
-          enable = false,
-        },
         icons = {
           show = {
             file = true,
             folder = true,
             folder_arrow = true,
             git = false,
+            modified = false,
+            diagnostics = true,
+            bookmarks = true,
           },
         },
+      },
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      git = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = false,
+      },
+      diagnostics = {
+        enable = true,
+        severity = {
+          min = vim.diagnostic.severity.WARN,
+          max = vim.diagnostic.severity.ERROR,
+        },
+      },
+      modified = {
+        enable = false,
       },
     },
   },
