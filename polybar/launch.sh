ffe9e9e2267907f2c375ddc4c50a26f8b3ffb451
danielefongo <danielefongo@gmail.com>
2023-10-19 13:54:41 +0200

    feat(polybar): simplify launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,12 +5,9 @@ CONFIG_FILE=$HOME/.config/polybar/config
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 export WLAN=$(ip link show | grep 'wl' | awk -F' ' '{print $2}' | sed 's/://g')
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar top -c $CONFIG_FILE &
-	done
-else
-	polybar top -c $CONFIG_FILE &
-fi
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar top -c $CONFIG_FILE &
+done
 
 sleep infinity
