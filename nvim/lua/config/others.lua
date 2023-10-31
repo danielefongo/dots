582dc6f188741abf2484f052d9798427d779c2e7
danielefongo <danielefongo@gmail.com>
2023-10-31 09:55:21 +0100

    feat(neovim): silently run tmux navigator

--- nvim/lua/config/others.lua
+++ nvim/lua/config/others.lua
@@ -10,10 +10,10 @@ return {
     config = function()
       vim.g.tmux_navigator_no_mappings = 1
 
-      vim.keymap.set("n", "<M-h>", ":TmuxNavigateLeft<cr>")
-      vim.keymap.set("n", "<M-j>", ":TmuxNavigateDown<cr>")
-      vim.keymap.set("n", "<M-k>", ":TmuxNavigateUp<cr>")
-      vim.keymap.set("n", "<M-l>", ":TmuxNavigateRight<cr>")
+      vim.keymap.set("n", "<M-h>", ":TmuxNavigateLeft<cr>", { silent = true })
+      vim.keymap.set("n", "<M-j>", ":TmuxNavigateDown<cr>", { silent = true })
+      vim.keymap.set("n", "<M-k>", ":TmuxNavigateUp<cr>", { silent = true })
+      vim.keymap.set("n", "<M-l>", ":TmuxNavigateRight<cr>", { silent = true })
     end,
   },
   {
