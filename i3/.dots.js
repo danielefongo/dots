36530f995e3e31e48ea49358217735696bf48c02
danielefongo <danielefongo@gmail.com>
2024-01-31 19:22:49 +0100

    improvement(i3): move mouse back to previous position after restart

--- i3/.dots.js
+++ i3/.dots.js
@@ -3,6 +3,6 @@ const { execSync } = require("child_process");
 module.exports = {
   match: "i3/**/*",
   apply: (_) => {
-    execSync("i3-msg restart || true");
+    execSync("i3restart");
   },
 };
