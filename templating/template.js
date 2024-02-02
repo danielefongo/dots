66d11c068fdaa47111225b11b38de4e7a27178d7
danielefongo <danielefongo@gmail.com>
2024-02-02 09:59:22 +0100

    refactor(theme): collapse data and computed

--- templating/template.js
+++ templating/template.js
@@ -14,7 +14,7 @@ function generateComputed (computed, full) {
       } else if (isObject(value)) {
         computed[key] = generateComputed(value, full)
       }
-    } catch (_) {}
+    } catch (_) { }
   })
 
   return computed
@@ -24,24 +24,6 @@ function isObject (item) {
   return item && typeof item === 'object' && !Array.isArray(item)
 }
 
-function mergeDeep (target, ...sources) {
-  if (!sources.length) return target
-  const source = sources.shift()
-
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) Object.assign(target, { [key]: {} })
-        mergeDeep(target[key], source[key])
-      } else {
-        Object.assign(target, { [key]: source[key] })
-      }
-    }
-  }
-
-  return mergeDeep(target, ...sources)
-}
-
 module.exports = function (content, template) {
   Object.entries(template.filters || {}).forEach(([key, value]) => {
     env.addFilter(key, (...args) => value(tinycolor, ...args))
@@ -49,12 +31,11 @@ module.exports = function (content, template) {
 
   let stringifiedData = JSON.stringify(template.data)
   while (true) {
-    generateComputed(template.computed, template.data)
-    template.data = mergeDeep(template.data, template.computed)
+    generateComputed(template.data, template.data)
 
     if (stringifiedData == JSON.stringify(template.data)) {
       if (stringifiedData.includes('{{')) {
-        throw `Cyclic dependency found, actual computed data: ${JSON.stringify(template.computed, null, 2)}`
+        throw `Invalid template, actual data: ${JSON.stringify(template.data, null, 2)}`
       }
       break
     }
--- templating/theme.js
+++ templating/theme.js
@@ -1,6 +1,6 @@
 const zen = false
 const scaleRatio = 1
-function scale (value) {
+function scale(value) {
   return Math.floor(value * scaleRatio)
 }
 
@@ -16,6 +16,19 @@ module.exports = {
     transparency: false,
     colors: {
       background: '#1f2430',
+      background_alt1: '{{ colors.background | light_darken(3) }}',
+      background_alt2: '{{ colors.background | light_lighten(3) }}',
+      magic_background:
+        '{{ colors.background if transparency else colors.background_alt1 }}',
+      grey1: '{{ colors.background | between(colors.foreground, 1, 10) }}',
+      grey2: '{{ colors.background | between(colors.foreground, 2, 10) }}',
+      grey3: '{{ colors.background | between(colors.foreground, 3, 10) }}',
+      grey4: '{{ colors.background | between(colors.foreground, 4, 10) }}',
+      grey5: '{{ colors.background | between(colors.foreground, 5, 10) }}',
+      grey6: '{{ colors.background | between(colors.foreground, 6, 10) }}',
+      grey7: '{{ colors.background | between(colors.foreground, 7, 10) }}',
+      grey8: '{{ colors.background | between(colors.foreground, 8, 10) }}',
+      grey9: '{{ colors.background | between(colors.foreground, 9, 10) }}',
       foreground: '#e9ebf0',
       red: '#f45c7f',
       orange: '#f78c6c',
@@ -24,6 +37,11 @@ module.exports = {
       cyan: '#9fd4ff',
       blue: '#6cbeff',
       magenta: '#c792ea'
+    },
+    theme: {
+      primary: '{{ colors.yellow }}',
+      secondary: '{{ colors.blue }}',
+      alert: '{{ colors.red }}'
     }
   },
   filters: {
@@ -35,27 +53,6 @@ module.exports = {
     between: (tc, color, secondColor, step, maxStep) => {
       const gradient = ((100 - 100 / (maxStep - 1)) / (maxStep - 1)) * step
       return tc.mix(color, secondColor, gradient).toHexString()
-    }
-  },
-  computed: {
-    colors: {
-      background_alt1: '{{ colors.background | light_darken(3) }}',
-      background_alt2: '{{ colors.background | light_lighten(3) }}',
-      magic_background: '{{ colors.background if transparency else colors.background_alt1 }}',
-      grey1: '{{ colors.background | between(colors.foreground, 1, 10) }}',
-      grey2: '{{ colors.background | between(colors.foreground, 2, 10) }}',
-      grey3: '{{ colors.background | between(colors.foreground, 3, 10) }}',
-      grey4: '{{ colors.background | between(colors.foreground, 4, 10) }}',
-      grey5: '{{ colors.background | between(colors.foreground, 5, 10) }}',
-      grey6: '{{ colors.background | between(colors.foreground, 6, 10) }}',
-      grey7: '{{ colors.background | between(colors.foreground, 7, 10) }}',
-      grey8: '{{ colors.background | between(colors.foreground, 8, 10) }}',
-      grey9: '{{ colors.background | between(colors.foreground, 9, 10) }}'
-    },
-    theme: {
-      primary: '{{ colors.yellow }}',
-      secondary: '{{ colors.blue }}',
-      alert: '{{ colors.red }}'
     }
   }
 }
