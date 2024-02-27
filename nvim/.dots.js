798626fe8d62d2695af45df4fe243aec28b84e1f
danielefongo <danielefongo@gmail.com>
2024-02-27 17:06:20 +0100

    feat(neovim): restore lockfile

--- nvim/.dots.js
+++ nvim/.dots.js
@@ -1,3 +1,10 @@
+const path = require("path");
+const { execSync } = require("child_process");
+const lockFilename = path.join(path.resolve(__dirname), "lazy-lock.json");
+
 module.exports = {
-  match: "nvim/**/*",
+  match: "nvim/**/*.lua",
+  apply: () => {
+    execSync(`ln -sf ${lockFilename} ./output/nvim/lazy-lock.json`);
+  },
 };
