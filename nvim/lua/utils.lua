447d8ea35be5c365b60fb7b1d093189b12ca7b3f
danielefongo <danielefongo@gmail.com>
2024-03-27 15:20:49 +0100

    chore(neovim): remove dead code

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,16 +1,5 @@
-key_opts = { noremap = true, silent = true }
-set_var = vim.api.nvim_set_var
-exec = vim.api.nvim_command
 opt = vim.opt
 fn = vim.fn
-
-function _G.set_key(mode, keys, command)
-  vim.api.nvim_set_keymap(mode, keys, command, key_opts)
-end
-
-function _G.set_buf_key(bufnr, mode, keys, command)
-  vim.api.nvim_buf_set_keymap(bufnr, mode, keys, command, key_opts)
-end
 
 function _G.close_win()
   return pcall(function()
