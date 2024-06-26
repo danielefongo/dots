b39cca2967a42117c9e82e8afe542c6fc0146502
danielefongo <danielefongo@gmail.com>
2024-06-26 13:41:04 +0200

    feat(wallpaper): use params on script

--- wallpaper/.dots.js
+++ wallpaper/.dots.js
@@ -3,7 +3,9 @@ const { execSync, exec } = require("child_process");
 module.exports = {
   match: [{ pattern: "wallpaper/*.js" }],
   apply: () => {
-    execSync("$(mise where nodejs)/bin/node wallpaper/index.js");
+    execSync(
+      "$(mise where nodejs)/bin/node wallpaper/index.js output/wallpaper/settings.js output/wallpaper/background.svg",
+    );
     exec("systemctl --user restart wallpaper");
   },
 };
