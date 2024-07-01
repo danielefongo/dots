920888532b653ff9948f5d809b490ed07db004d5
danielefongo <danielefongo@gmail.com>
2024-07-01 17:24:01 +0200

    feat(nix): create i3-session target

--- nix-files/home-manager/modules/i3.nix
+++ nix-files/home-manager/modules/i3.nix
@@ -10,4 +10,21 @@
 
   xdg.configFile."i3".source = config.lib.file.mkOutOfStoreSymlink "${dots_path}/output/i3";
   xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink "${dots_path}/output/rofi";
+
+  systemd.user.targets = {
+    i3-session = {
+      Unit = {
+        Description = "i3 session";
+        BindsTo = "graphical-session.target";
+        Wants = [
+          "polybar.service"
+          "picom.service"
+          "wallpaper.service"
+          "redshift.service"
+          "xsettingsd.service"
+          "theme.service"
+        ];
+      };
+    };
+  };
 }
