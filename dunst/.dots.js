f8eb80b8b7a930ea659c089c8409cbe4fe9c64ca
danielefongo <danielefongo@gmail.com>
2024-07-02 23:02:13 +0200

    fix(dunst): force kill other notification daemons

--- dunst/.dots.js
+++ dunst/.dots.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 module.exports = {
-  match: [{ pattern: "dunst/**/*" }],
+  match: [{ pattern: "dunst/dunstrc" }],
   apply: (_) => {
     exec("systemctl --user restart dunst && notify-send Dunst restarted");
   },
--- dunst/default.nix
+++ dunst/default.nix
@@ -1,10 +1,17 @@
 { pkgs, config, dots_path, ... }:
 
-{
-  home.packages = with pkgs; [
-    dunst
-  ];
+let
+  dunstWrapper = pkgs.writeShellScriptBin "dunst" ''
+    error=$(${pkgs.dunst}/bin/dunst 2>&1 > /dev/null)
 
+    if [[ $error == *"Cannot acquire 'org.freedesktop.Notifications'"* ]]; then
+        pid=$(echo $error | grep -oP "PID '\K[0-9]+")
+        kill $pid
+        ${pkgs.dunst}/bin/dunst > /dev/null
+    fi
+  '';
+in
+{
   xdg.configFile."dunst".source = config.lib.file.mkOutOfStoreSymlink "${dots_path}/output/dunst";
 
   systemd.user.services = {
@@ -14,7 +21,7 @@
       };
 
       Service = {
-        ExecStart = "${pkgs.dunst}/bin/dunst";
+        ExecStart = "${dunstWrapper}/bin/dunst";
         Restart = "always";
       };
     };
