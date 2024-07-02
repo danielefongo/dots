838b209fa6cb3b944df9d3bf809f3e4495cbac7e
danielefongo <danielefongo@gmail.com>
2024-07-02 18:48:59 +0200

    feat(git): add nix module

---
+++ git/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, home, config, dots_path, ... }:
+
+{
+  home.packages = with pkgs; [
+    git
+    delta
+  ];
+
+  xdg.configFile."git".source = config.lib.file.mkOutOfStoreSymlink "${dots_path}/output/git";
+}
