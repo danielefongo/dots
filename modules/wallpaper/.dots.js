1f74546f835e2120ee97f924d59d896dd40e25cd
danielefongo <danielefongo@gmail.com>
2024-07-09 06:50:06 +0200

    fix(wallpaper): readd dots.js

---
+++ modules/wallpaper/.dots.js
@@ -0,0 +1,8 @@
+const { exec } = require("child_process");
+
+module.exports = {
+  match: [{ pattern: "*.js", to: "wallpaper" }],
+  apply: () => {
+    exec("systemctl --user restart wallpaper");
+  },
+};
