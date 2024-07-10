6ba80a52f9cda5d2892497d6b0d65c48cafb13c1
danielefongo <danielefongo@gmail.com>
2024-07-10 16:01:18 +0200

    feat(playerctl): add nix module and custom binary

---
+++ modules/playerctl/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+let
+  playerctlStatus = pkgs.writeShellScriptBin "playerctl-status" ''
+    #!/bin/env bash
+    PLAYER="$1"
+    NOPLAYER_FORMAT=$(echo "$2" | sed -E 's/<([^>]+)>/{{ \1 }}/g')
+    STOPPED_FORMAT=$(echo "$3" | sed -E 's/<([^>]+)>/{{ \1 }}/g')
+    PLAY_FORMAT=$(echo "$4" | sed -E 's/<([^>]+)>/{{ \1 }}/g')
+    PAUSE_FORMAT=$(echo "$5" | sed -E 's/<([^>]+)>/{{ \1 }}/g')
+
+    PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
+    EXIT_CODE=$?
+
+    if [ $EXIT_CODE -eq 0 ]; then
+    	STATUS=$PLAYERCTL_STATUS
+    else
+    	STATUS="No player is running"
+    fi
+
+    if [ "$STATUS" = "Stopped" ]; then
+    	echo "$STOPPED_FORMAT"
+    elif [ "$STATUS" = "Paused" ]; then
+    	playerctl --player=$PLAYER metadata --format "$PAUSE_FORMAT"
+    elif [ "$STATUS" = "No player is running" ]; then
+    	echo "$NOPLAYER_FORMAT"
+    else
+    	playerctl --player=$PLAYER metadata --format "$PLAY_FORMAT"
+    fi
+  '';
+in
+{
+  home.packages = [
+    pkgs.playerctl
+    playerctlStatus
+  ];
+}
