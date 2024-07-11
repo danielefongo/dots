d1c262e9c3f98ea7d29a0ed6a62ea772e124e201
danielefongo <danielefongo@gmail.com>
2024-07-11 23:48:44 +0200

    feat(hyprland): add dunst and flameshot

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -26,6 +26,8 @@ in
         Description = "hyprland session";
         BindsTo = "graphical-session.target";
         Wants = [
+          "dunst.service"
+          "flameshot.service"
         ];
       };
     };
