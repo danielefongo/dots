5207768646fbe6832aa8c4bc9a005ffffead0c3b
danielefongo <danielefongo@gmail.com>
2024-08-20 20:48:18 +0200

    fix(wallpaper): do not check if service is active since it is oneshot

--- modules/wallpaper/.dots.js
+++ modules/wallpaper/.dots.js
@@ -3,10 +3,6 @@ const { exec } = require("child_process");
 module.exports = {
   match: [{ pattern: "*.js", to: "wallpaper" }],
   apply: () => {
-    exec("systemctl --user is-active wallpaper.service", (_, stdout) => {
-      if (stdout.trim() === "active") {
-        exec("systemctl --user restart wallpaper.service");
-      }
-    });
+    exec("systemctl --user restart wallpaper.service");
   },
 };
