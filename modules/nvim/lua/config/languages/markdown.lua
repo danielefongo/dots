728681b0642fe8bf97fb25e79fd99d246552b3c9
danielefongo <danielefongo@gmail.com>
2024-09-10 08:03:56 +0200

    feat(neovim): format injected languages in markdown

--- modules/nvim/lua/config/languages/markdown.lua
+++ modules/nvim/lua/config/languages/markdown.lua
@@ -9,7 +9,7 @@ return {
     "stevearc/conform.nvim",
     opts = function(_, opts)
       table.insert(opts.mason_sources, "markdownlint")
-      opts.options.formatters_by_ft.markdown = { "markdownlint" }
+      opts.options.formatters_by_ft.markdown = { "markdownlint", "injected" }
     end,
   },
 }
