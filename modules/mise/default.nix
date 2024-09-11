f9f8ca5524898de8bc5cea07af56ae41d99ed8d7
danielefongo <danielefongo@gmail.com>
2024-09-11 18:12:28 +0200

    feat(mise): prevent overriding globals using nix

--- modules/mise/default.nix
+++ modules/mise/default.nix
@@ -1,11 +1,23 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     zlib
     ncurses
-    mise
   ];
 
-  xdg.configFile."mise".source = lib.outLink "mise";
+  programs.mise = {
+    enable = true;
+    globalConfig = {
+      tools = {
+        erlang = "latest";
+        elixir = "latest";
+        elm = "latest";
+        lua = "latest";
+        nodejs = "latest";
+        python = "latest";
+        rust = "latest";
+      };
+    };
+  };
 }
