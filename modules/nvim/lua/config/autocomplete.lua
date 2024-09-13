636d6a73e0785094ad79880b19c27393581e418d
danielefongo <danielefongo@gmail.com>
2024-09-13 08:47:46 +0200

    feat(neovim): add nvim-snippets

--- modules/nvim/lua/config/autocomplete.lua
+++ modules/nvim/lua/config/autocomplete.lua
@@ -62,6 +62,44 @@ return {
   },
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "garymjr/nvim-snippets",
+        opts = { friendly_snippets = true },
+        dependencies = { "rafamadriz/friendly-snippets" },
+      },
+    },
+    opts = function(_, opts)
+      opts.snippet = {
+        expand = function(item)
+          return vim.snippet.expand(item.body)
+        end,
+      }
+      table.insert(opts.sources, { name = "snippets" })
+    end,
+    keys = {
+      {
+        "<Tab>",
+        function()
+          return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+      {
+        "<S-Tab>",
+        function()
+          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
     dependencies = { "onsails/lspkind.nvim" },
     opts = function(_, opts)
       local lspkind = require("lspkind")
