44ac44b9cef08ffb557c267283d851e7f322d94a
danielefongo <danielefongo@gmail.com>
2024-09-15 08:49:08 +0200

    feat(webapps): add netflix

--- modules/webapps/default.nix
+++ modules/webapps/default.nix
@@ -55,5 +55,14 @@ in
       music = lib.outFile "webapps/music/spotify.js";
       userAgent = userAgent;
     })
+    (webApp {
+      name = "Netflix";
+      site = "https://www.netflix.com";
+      icon = (pkgs.fetchurl {
+        url = "https://img.icons8.com/color/480/netflix.png";
+        sha256 = "sha256-3L1asm7dLDLA6CQhuFpzUxDzhTj3Du2/M8K6KLA+ryM=";
+      }).outPath;
+      userAgent = userAgent;
+    })
   ];
 }
