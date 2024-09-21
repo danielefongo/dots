4959f66ddbab0b3811ffe6b7afb6ae819038b1ad
danielefongo <danielefongo@gmail.com>
2024-09-21 08:26:07 +0200

    fix(neovim): do not reload session if lazy window is open

--- modules/nvim/lua/config/sessions.lua
+++ modules/nvim/lua/config/sessions.lua
@@ -3,13 +3,12 @@ return {
     "folke/persistence.nvim",
     event = "BufReadPre",
     init = function()
-      vim.api.nvim_create_autocmd("VimEnter", {
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyDone",
         group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
         callback = function()
-          if not vim.tbl_contains({
-            "gitcommit",
-            "gitrebase",
-          }, vim.bo.filetype) then
+          local invalid_buf = vim.tbl_contains({ "gitcommit", "gitrebase", "lazy" }, vim.bo.filetype)
+          if not invalid_buf then
             require("persistence").load()
           end
         end,
