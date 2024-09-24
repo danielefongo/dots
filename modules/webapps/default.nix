c440a027e5877e3dcec89cc321e3a994524a015c
danielefongo <danielefongo@gmail.com>
2024-09-24 20:39:57 +0200

    fix(webapps): right user agent for netflix

--- modules/webapps/default.nix
+++ modules/webapps/default.nix
@@ -2,7 +2,8 @@
 
 let
   webApp = import ./package.nix { inherit pkgs lib; };
-  userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
+  chromeUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
+  firefoxUserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0";
 in
 {
   home.packages = [
@@ -14,7 +15,7 @@ in
         url = "https://img.icons8.com/color-glass/480/slack-new.png";
         sha256 = "sha256-O7C1z66smLZShfgUw1L3rXCgb/TgRqLqOv7vgEQAei4=";
       }).outPath;
-      userAgent = userAgent;
+      userAgent = chromeUserAgent;
       uriParser = ''(uri) => {
         const parsedUrl = new URL(uri);
         const params = new URLSearchParams(parsedUrl.search);
@@ -34,7 +35,7 @@ in
         sha256 = "sha256-Wg/jU2cGcLl7WJCDOvld0KjwSfByiFSXsZkUM/J43GU=";
       }).outPath;
       css = lib.outFile "webapps/style/telegram.css";
-      userAgent = userAgent;
+      userAgent = chromeUserAgent;
     })
     (webApp {
       name = "WhatsApp";
@@ -43,7 +44,7 @@ in
         url = "https://img.icons8.com/color/480/whatsapp--v1.png";
         sha256 = "sha256-b7D6VTLW/X/To1iSG0++ydGaU2WS91+6M48oY2arYL0=";
       }).outPath;
-      userAgent = userAgent;
+      userAgent = chromeUserAgent;
     })
     (webApp {
       name = "Spotify";
@@ -53,7 +54,7 @@ in
         sha256 = "sha256-vYs3+sJDxedD3BOMPeMDsUX2f42NIe8gHjTdPUXb35s=";
       }).outPath;
       music = lib.outFile "webapps/music/spotify.js";
-      userAgent = userAgent;
+      userAgent = chromeUserAgent;
     })
     (webApp {
       name = "Netflix";
@@ -62,7 +63,7 @@ in
         url = "https://img.icons8.com/color/480/netflix.png";
         sha256 = "sha256-3L1asm7dLDLA6CQhuFpzUxDzhTj3Du2/M8K6KLA+ryM=";
       }).outPath;
-      userAgent = userAgent;
+      userAgent = firefoxUserAgent;
     })
   ];
 }
