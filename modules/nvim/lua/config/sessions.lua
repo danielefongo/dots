39360ad698da9332929a847b83b7d542b9fb0c7e
danielefongo <danielefongo@gmail.com>
2024-09-26 18:01:04 +0200

    feat(neovim): use persisted insted of persistence

--- modules/nvim/lua/config/sessions.lua
+++ modules/nvim/lua/config/sessions.lua
@@ -1,35 +1,17 @@
 return {
   {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    init = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyDone",
-        group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
-        callback = function()
-          local invalid_buf = vim.tbl_contains({ "gitcommit", "gitrebase", "lazy" }, vim.bo.filetype)
-          if not invalid_buf then
-            require("persistence").load()
-          end
-        end,
-        nested = true,
-      })
-    end,
-    config = function()
-      local persistence = require("persistence")
-      persistence.setup()
-
-      persistence.delete_current_and_stop = function()
-        if persistence.current then
-          os.remove(persistence.current())
-        end
-        persistence.stop()
-      end
-    end,
+    "olimorris/persisted.nvim",
+    lazy = false,
+    opts = {
+      autostart = true,
+      autoload = true,
+      should_save = function()
+        return not vim.tbl_contains({ "gitcommit", "gitrebase", "lazy" }, vim.bo.filetype)
+      end,
+    },
     keys = {
-      { "<leader>sd", ":lua require('persistence').delete_current_and_stop()<cr>", desc = "delete", silent = true },
-      { "<leader>sl", ":lua require('persistence').load()<cr>", desc = "load", silent = true },
-      { "<leader>ss", ":lua require('persistence').save()<cr>", desc = "save", silent = true },
+      { "<leader>sd", ":lua require('persisted').delete()<cr>", desc = "delete", silent = true },
+      { "<leader>sl", ":lua require('persisted').load()<cr>", desc = "delete", silent = true },
     },
   },
 }
