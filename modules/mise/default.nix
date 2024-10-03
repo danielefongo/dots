572f3b985d6fc464d65734986e002dcb91bd7480
danielefongo <danielefongo@gmail.com>
2024-10-03 21:12:06 +0200

    feat(mise): install languages using nix

--- modules/mise/default.nix
+++ modules/mise/default.nix
@@ -1,24 +1,29 @@
 { pkgs, ... }:
 
+let
+  mkMisePackage = import ./package.nix { inherit pkgs; };
+in
 {
   home.packages = with pkgs; [
-    zlib
-    ncurses
+    mise
+    (mkMisePackage { name = "node"; version = "22.9.0"; })
+    (mkMisePackage { name = "elm"; version = "0.19.1"; })
+    (mkMisePackage { name = "lua"; version = "5.4.7"; buildInputs = [ unzip ]; })
+    (mkMisePackage { name = "python"; version = "3.12.7"; })
+    (mkMisePackage { name = "rust"; version = "1.81.0"; })
+    (mkMisePackage { name = "elixir"; version = "1.17.3-otp-27"; buildInputs = [ unzip ]; })
+    (mkMisePackage {
+      name = "erlang";
+      version = "27.1.1";
+      buildInputs = [ ncurses git perl openssl ];
+      env = {
+        KERL_CONFIGURE_OPTIONS = lib.concatStrings ([ "--without-javac " ]
+          ++ [ "--with-ssl=${lib.getOutput "out" openssl} " ]
+          ++ [ "--with-ssl-incl=${lib.getDev openssl}" ]);
+      };
+    })
+    (mkMisePackage { name = "ruby"; version = "3.3.5"; buildInputs = [ libyaml ]; })
   ];
 
-  programs.mise = {
-    enable = true;
-    globalConfig = {
-      tools = {
-        erlang = "latest";
-        elixir = "latest";
-        elm = "latest";
-        lua = "latest";
-        nodejs = "latest";
-        python = "latest";
-        rust = "latest";
-        ruby = "latest";
-      };
-    };
-  };
+  xdg.configFile."mise/config.toml".text = "";
 }
---
+++ modules/mise/package.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+({ name, version ? "latest", buildInputs ? [ ], env ? { } }:
+with pkgs; stdenv.mkDerivation ({
+  pname = name;
+  version = version;
+  src = runCommand "empty" { } "mkdir -p $out";
+  buildInputs = [ curl ] ++ buildInputs;
+  installPhase = ''
+    export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+    MISE_VERBOSE=1 HOME=$out ${mise}/bin/mise install ${name}@${version} -y
+    mkdir -p $out/${name}/bin
+    cp -r $out/.local/share/mise/installs/${name}/${version}/* $out/${name}/
+    mkdir -p $out/bin
+    for file in $out/${name}/bin/*; do
+      ln -s ../${name}/bin/$(basename $file) $out/bin/$(basename $file)
+    done
+  '';
+} // env))
