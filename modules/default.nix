a1539dc6568ef92d9191d2613183d386a907c39c
danielefongo <danielefongo@gmail.com>
2024-11-22 12:05:48 +0100

    feat(earlyoom): add service to clean RAM

--- modules/default.nix
+++ modules/default.nix
@@ -6,6 +6,7 @@
     ./discord
     ./docker
     ./dunst
+    ./earlyoom
     ./essentials
     ./firefox
     ./flameshot
---
+++ modules/earlyoom/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    earlyoom
+  ];
+
+  systemd.user.services = {
+    earlyoom = {
+      Unit = {
+        Description = "Early OOM";
+      };
+
+      Service = {
+        ExecStart = "${pkgs.earlyoom}/bin/earlyoom";
+        Restart = "on-failure";
+        RestartSec = 2;
+      };
+    };
+  };
+}
