c3d5d9bcc83e9a716d3d58dc6250d162978dd0f5
danielefongo <danielefongo@gmail.com>
2025-01-10 08:05:17 +0100

    feat(neovim): do not store folds on session

--- modules/nvim/lua/config/sessions.lua
+++ modules/nvim/lua/config/sessions.lua
@@ -9,6 +9,14 @@ return {
         return not vim.tbl_contains({ "gitcommit", "gitrebase", "lazy" }, vim.bo.filetype)
       end,
     },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "PersistedSavePre",
+        callback = function()
+          vim.o.sessionoptions = "buffers,tabpages,winsize"
+        end,
+      })
+    end,
     keys = {
       { "<leader>sd", ":lua require('persisted').delete()<cr>", desc = "delete", silent = true },
       { "<leader>sl", ":lua require('persisted').load()<cr>", desc = "delete", silent = true },
