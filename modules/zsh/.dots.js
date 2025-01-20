6c6a89c3e16e3f67f4123aacf24e4ab3311bc3bf
danielefongo <danielefongo@gmail.com>
2025-01-20 18:01:42 +0100

    feat(zsh): use sheldon instead of antigen

--- modules/zsh/.dots.js
+++ modules/zsh/.dots.js
@@ -2,5 +2,6 @@ module.exports = {
   match: [
     { pattern: ".zshrc", to: "zsh" },
     { pattern: ".custom_zshrc", to: "zsh" },
+    { pattern: "plugins.toml", to: "sheldon" },
   ],
 };
--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,8 +1,12 @@
 { lib, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ zsh ];
+  home.packages = with pkgs; [
+    zsh
+    sheldon
+  ];
 
   home.file.".zshrc".source = lib.outLink "zsh/.zshrc";
   home.file.".custom_zshrc".source = lib.outLink "zsh/.custom_zshrc";
+  xdg.configFile."sheldon".source = lib.outLink "sheldon";
 }
