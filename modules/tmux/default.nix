bf74366a8b550f1e966bdd5d9c37d369048f4eda
danielefongo <danielefongo@gmail.com>
2025-01-27 16:13:26 +0100

    feat(tmux+alacritty): add script to open links on nvim

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -1,10 +1,14 @@
 { lib, pkgs, ... }:
 
+let
+  run_on_nvim = (pkgs.writeShellScriptBin "run_on_nvim" (builtins.readFile ./run_on_nvim.sh));
+in
 {
   home.packages = with pkgs; [
     entr
     tmux
     tmuxinator
+    run_on_nvim
   ];
 
   xdg.configFile."tmux/tmux.conf".source = lib.outLink "tmux/tmux.conf";
