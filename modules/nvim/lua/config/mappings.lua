3dbe920826d2a53950b3d01fb2783ed2df548866
danielefongo <danielefongo@gmail.com>
2025-02-21 13:58:29 +0100

    feat(neovim): add select-all mapping

--- modules/nvim/lua/config/mappings.lua
+++ modules/nvim/lua/config/mappings.lua
@@ -57,6 +57,7 @@ return {
         { "<a-q>", ":lua close_win()<cr>", desc = "close buffer", silent = true },
         { "<c-e>", "<c-y>", desc = "scroll up", silent = true },
         { "<c-n>", "<c-e>", desc = "scroll down", silent = true },
+        { "<c-a>", "gg0vG$", desc = "select all", noremap = true, silent = true },
       },
     },
   },
