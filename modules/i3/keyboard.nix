0d81fedd44614ed3066693361363b30c78495791
danielefongo <danielefongo@gmail.com>
2025-02-28 09:19:28 +0100

    feat(i3): add i3-keyboard service to automatically set the speed on plug-in

---
+++ modules/i3/keyboard.nix
@@ -0,0 +1,37 @@
+{ lib, pkgs, ... }:
+
+{
+  systemd.user.services = {
+    i3-keyboard = {
+      Unit = {
+        Description = "i3 keyboard speed setter";
+        PartOf = [ "i3-session.target" ];
+      };
+
+      Install = {
+        WantedBy = [ "i3-session.target" ];
+      };
+
+      Service = {
+        ExecStart = lib.scriptToBinary ''
+          #!/bin/bash
+          autorandr -l default
+          xset r rate 200 60
+
+          udevadm monitor --environment --udev | while read -r line; do
+            if echo "$line" | grep -q "change"; then
+              while read -r detail; do
+                if echo "$detail" | grep -q "ID_INPUT_KEYBOARD=1"; then
+                  xset r rate 200 60
+                  break
+                fi
+              done
+            fi
+          done
+        '';
+        Restart = "on-failure";
+        RestartSec = 2;
+      };
+    };
+  };
+}
