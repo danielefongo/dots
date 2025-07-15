e93cafc3a9166800a8e1ecdf69d6a7a3045f385a
danielefongo <danielefongo@gmail.com>
2025-07-15 11:55:44 +0200

    refactor(polybar): use single module with better script for cpu temp

--- home/modules/polybar/scripts.nix
+++ home/modules/polybar/scripts.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 let
   cpu_temp = pkgs.writeShellScriptBin "cpu-temp" ''
@@ -6,26 +6,37 @@ let
 
     sensors="${pkgs.lm_sensors}/bin/sensors"
 
-    cpu_temp=$($sensors | awk '/^Tctl:/ {gsub(/\+|°C/, "", $2); print int($2); exit}')
+    cpu_temp=$($sensors | awk '/CPU:/ {gsub(/\+|°C/, "", $2); print int($2); exit}')
     if [ -z "$cpu_temp" ]; then
-      cpu_temp=$($sensors | awk '/^Package id 0:/ {gsub(/\+|°C/, "", $4); print int($4); exit}')
+      cpu_temp=$($sensors | awk '/Package id 0:/ {gsub(/\+|°C/, "", $4); print int($4); exit}')
     fi
     if [ -z "$cpu_temp" ]; then
       cpu_temp=$($sensors | awk '/temp1:/ {gsub(/\+|°C/, "", $2); print int($2); exit}')
     fi
 
     if [ -z "$cpu_temp" ]; then
-      exit 0
+      echo "N/A"
+      exit 1
     fi
 
-    operator="$1"
-    threshold="$2"
+    threshold="$1"
+    ok_format=$(echo "$2" | sed -E 's/<([^>]+)>/{{ \1 }}/g')
+    warn_format=$(echo "$3" | sed -E 's/<([^>]+)>/{{ \1 }}/g')
 
-    if [ "$cpu_temp" "$operator" "$threshold" ]; then
-      echo "$cpu_temp"
+    if [ -z "$threshold" ] || [ -z "$ok_format" ] || [ -z "$warn_format" ]; then
+      echo "Usage: cpu-temp <threshold> <ok_format> <warn_format>"
+      exit 1
+    fi
+
+    output=""
+
+    if [ "$cpu_temp" -lt "$threshold" ]; then
+      output="$ok_format"
     else
-      exit 0
+      output="$warn_format"
     fi
+
+    echo "$output" | sed "s/{{ temp }}/$cpu_temp/g"
   '';
 in
 {
