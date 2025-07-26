daa79557ea845fa5c1d84837cad7f90f205b3337
danielefongo <danielefongo@gmail.com>
2025-07-26 09:11:26 +0200

    feat(neovim): update deps and fix lazy-lock nix link

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -24,7 +24,7 @@
 
   xdg.configFile."nvim/init.lua".source = lib.outLink "nvim/init.lua";
   xdg.configFile."nvim/lua".source = lib.outLink "nvim/lua";
-  xdg.configFile."nvim/lazy-lock.json".source = lib.dotLink "modules/nvim/lazy-lock.json";
+  xdg.configFile."nvim/lazy-lock.json".source = lib.dotLink "home/modules/nvim/lazy-lock.json";
 
   home.packages = [
     pkgs.python3 # mason
