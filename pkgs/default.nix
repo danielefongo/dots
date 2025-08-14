78ac91c589a8b3b1ccd403cc7ba8a7f2af3184f7
danielefongo <danielefongo@gmail.com>
2025-08-14 14:43:10 +0200

    feat(tmux): do not use custom derivation

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,7 +7,6 @@
 
 (self: super: {
   ocr = pkgs.callPackage ./ocr.nix { pkgs = super; };
-  tmux = pkgs.callPackage ./tmux.nix { pkgs = super; };
   firefox-addons = pkgs.nur.repos.rycee.firefox-addons;
   plover = pkgs.callPackage ./plover.nix {
     inherit inputs;
