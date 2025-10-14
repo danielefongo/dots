fddb87f0f7298838d6639ee1e43cdc0f77a11129
danielefongo <danielefongo@gmail.com>
2025-10-14 10:39:17 +0200

    chore(git): remove unused config file

--- home/modules/git/default.nix
+++ home/modules/git/default.nix
@@ -10,13 +10,11 @@
       { path = lib.outLink "git/delta"; }
       { path = lib.outLink "git/extra_config"; }
     ];
-
     extraConfig = {
       user = {
         name = "danielefongo";
         email = "danielefongo@gmail.com";
       };
-
       core = {
         editor = "nvim";
         excludesfile = builtins.toString (lib.outLink "git/ignore");
