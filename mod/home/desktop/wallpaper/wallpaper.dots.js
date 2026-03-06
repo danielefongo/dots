deae2b9cc87c1c04123e89f4bd88138e576c21a6
danielefongo <danielefongo@gmail.com>
2026-03-06 13:54:52 +0100

    fix(desktop.wallpaper): restart service on changes

--- mod/home/desktop/wallpaper/wallpaper.dots.js
+++ mod/home/desktop/wallpaper/wallpaper.dots.js
@@ -3,6 +3,6 @@ const { exec } = require("child_process");
 module.exports = {
   match: [{ pattern: "background.svg", to: "wallpaper" }],
   apply: () => {
-    exec("systemctl --user start wallpaper.service");
+    exec("systemctl --user restart wallpaper.service");
   },
 };
