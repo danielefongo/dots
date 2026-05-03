bd9dadaa805aba4d1aa26f6e53592a58e09a9fef
danielefongo <danielefongo@gmail.com>
2026-05-03 19:39:16 +0200

    feat(theme): rename sharing to galaxy

--- themes/galaxy.js
+++ themes/galaxy.js
@@ -2,10 +2,9 @@ let baseTheme = require("./base.js");
 
 module.exports = {
   ...baseTheme,
-  name: "Sharing",
+  name: "Galaxy",
   data: {
     ...baseTheme.data,
-    fontSize: 16,
     contrast: 8,
   },
 };
